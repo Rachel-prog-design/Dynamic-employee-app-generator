fe41ed8d11f970aef1fd302559a506102204f68e
Rachel-prog-design <rachelnyinawabana7@gmail.com>
2026-03-12 11:46:33 +0200

    router installation, home page and employeecard

---
+++ employee app/src/components/EmployeeCard.jsx
@@ -0,0 +1,17 @@
+function EmployeeCard({ employee }) {
+
+  const { id, name, email, phone, website, company } = employee;
+
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      <p><strong>ID:</strong> {id}</p>
+      <p><strong>Email:</strong> {email}</p>
+      <p><strong>Phone:</strong> {phone}</p>
+      <p><strong>Website:</strong> {website}</p>
+      <p><strong>Company:</strong> {company.name}</p>
+    </div>
+  );
+}
+
+export default EmployeeCard;
